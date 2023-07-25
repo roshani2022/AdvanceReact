5d2593e25a1f3622f2d0b35a5ee9a98b908799e0
roshani 2022 <rosh.gupta17@gmail.com>
2023-07-25 23:40:34 +0530

    advncedReactTask14

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -6,7 +6,6 @@ import ChartBar from "./ChartBar";
 const Chart = (props) => {
     const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value)
     const totalMaximum = Math.max(...dataPointValues)
-    console.log(totalMaximum)
   return (
     <div className="chart">
       {props.dataPoints.map((dataPoint) => (
